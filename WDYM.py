49f32882cc37fdd4972c358c374ad2126a57a360
David Leonel Nuñez Elvir <davidelvir2007@hotmail.com>
2019-08-09 10:34:57 -0600

    correción de lectura y arreglar quiebres

--- WDYM.py
+++ WDYM.py
@@ -16,25 +16,57 @@ m = ['j','n','m',',']
 comandos = ['ls','rm']
 while True:
     comando = input(">>>")
-    #print(repr(comando))
-    if comando == '':
+    if comando == '': #evitar quiebre con enter
         pass
-    elif(comando == 'file'):
+    elif(comando == 'file'): #crear archivos random para borrar
         os.system("type NUL > " + randomString()+".txt")
-    elif(comando == "ex" or comando == "exit"):
+    elif(comando == "ex" or comando == "exit"):#salida
         print("Adios :D")
         break
     else:
-        if comando[:2] in comandos:
-            if((comando[0] in l or comando[0] in s) and (comando[1] in l or comando[1] in s)):
-                os.system("dir")
-            elif((comando[0] in r or comando[0] in m) and (comando[1] in r or comando[1] in m)):
-                #print("del")
-                print(os.system("del"+comando[2::]))
-        else:
-            if((comando[0] in l or comando[0] in s) and (comando[1] in l or comando[1] in s)):
-                res = input("Did you mean ls ? (Y/N)")
-                if(res == 'Y' or res == 'y'):
-                    comandos.append(comando)                    
+        #si se guardó el comando solo ejecutarlo
+        if(len(comando) > 1):
+            if comando[:2] in comandos:
+                if(comando[0] in l or comando[0] in s):
                     os.system("dir")
+                elif(comando[0] in r or comando[0] in m):
+                    os.system("del"+comando[2::])
+            else:
+                #ls
+                if(comando[0] in l):
+                    if(comando[1] in s):
+                        res = input("Did you mean ls ? (Y/N)")
+                        if(res == 'Y' or res == 'y'):
+                            comandos.append(comando[:2])                    
+                            os.system("dir")
+                    else:
+                        print("Este comando no se reconoce")
+                elif(comando[0] in s):
+                    if(comando[1] in l):
+                        res = input("Did you mean ls ? (Y/N)")
+                        if(res == 'Y' or res == 'y'):
+                            comandos.append(comando[:2])                    
+                            os.system("dir")
+                    else:
+                        print("Este comando no se reconoce")
+
+                #rm
+                elif(comando[0] in r):
+                    if(comando[1] in m):
+                        res = input("Did you mean rm ? (Y/N)")
+                        if(res == 'Y' or res == 'y'):
+                            comandos.append(comando[:2])                    
+                            os.system("del"+comando[2::])
+                    else:
+                        print("Este comando no se reconoce")
+                elif(comando[0] in m):
+                    if(comando[1] in r):
+                        res = input("Did you mean rm ? (Y/N)")
+                        if(res == 'Y' or res == 'y'):
+                            comandos.append(comando[:2])                    
+                            os.system("del"+comando[2::])
+                    else:
+                        print("Este comando no se reconoce")
                 
+        else:
+            print("Este comando no se reconoce")
